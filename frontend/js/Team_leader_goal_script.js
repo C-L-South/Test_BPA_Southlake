9308226d012252ed23d82dc2ca76e412bf13add2
C-L-South <codyli9219@gmail.com>
2025-01-10 02:54:55 +0800

    Update Team_leader_goal_script.js

--- frontend/js/Team_leader_goal_script.js
+++ frontend/js/Team_leader_goal_script.js
@@ -122,7 +122,7 @@ document.addEventListener('DOMContentLoaded', () => {
       }
   });
     HabitTracker.addEventListener('click',() => {
-    window.location.href = '/website_screens/goal_page/Team_leader_home_index.html';
+    window.location.href = '/website_screens/home_page/Team_leader_home_index.html';
   });
   goalSettingBtn.addEventListener('click', function () {
     window.location.href = '/website_screens/goal_page/Team_leader_goal_index.html';
