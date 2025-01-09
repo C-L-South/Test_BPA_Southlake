39a9022d65bfc436ebf2953d5de7d42f0ea4a4c0
C-L-South <codyli9219@gmail.com>
2025-01-10 02:53:44 +0800

    Update Team_member_chart_script.js

--- frontend/js/Team_member_chart_script.js
+++ frontend/js/Team_member_chart_script.js
@@ -148,7 +148,7 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
       HabitTracker.addEventListener('click',() => {
-    window.location.href = '/website_screens/goal_page/Team_member_home_index.html';
+    window.location.href = '/website_screens/home_page/Team_member_home_index.html';
   });
     TeamGoalsBtn.addEventListener('click', () => {
       window.location.href = '/website_screens/goal_page/Team_member_goal_index.html';
